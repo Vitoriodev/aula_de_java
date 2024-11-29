f1c0216debde2a6af72f6c6e17d128fa78d92dce
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-11-29 20:03:12 -0300

    estou testando a variavel

--- primeiro_codigo.java
+++ primeiro_codigo.java
@@ -1,10 +1,7 @@
 public class primeiro_codigo{
     public static void main(String[] args){
-
         int comovai = 30;
 
         System.out.println("oi como você esta?"+comovai);
-        
-
     }
 }
