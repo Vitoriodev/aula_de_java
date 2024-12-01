552e70050adbedfb69eb1340487445e0203e9231
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-11-30 23:38:26 -0300

    5.5

---
+++ aprendendo/src/apredendo2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class apredendo2 {
+    public static void main(String[] args){
+
+        /* System.out.println("Tipos primitivos casting");
+
+        //isso é um casting, estou forçando um valor muito grande em uma variavel pequena
+        int oi = (int) 1000L;
+
+        String como = "pode colocar um grande texto";
+
+        System.out.println(como);*/
+
+        String nome = "Vitorio";
+        String endereco = "imperial";
+        String salario = "1500";
+        String data = "30/11";
+
+        System.out.print("Eu " +nome+" morando no endereço "+endereco);
+        System.out.println(", confirmo que o salário de "+salario+" reais na data "+data);
+
+        // isso é um input, para receber uma informação do usuario.
+        Scanner input = new Scanner(System.in);
+
+        int numero = input.nextInt();
+
+        System.out.println(numero);
+
+
+    }
+}
